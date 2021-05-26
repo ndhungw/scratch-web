d41acc58e55f094abc924650ebaa4b3a73031f40
linhntt <linhntt317@gmail.com>
2021-05-26 19:33:02 +0700

    count number image upload

--- src/pages/main/EditRecipe/ImageGallery.js
+++ src/pages/main/EditRecipe/ImageGallery.js
@@ -14,81 +14,91 @@ import image_sm from "../../../assets/images/ImgGallery_sm.png";
 import useTypographyStyles from "../../../assets/styles/useTypographyStyles";
 import COLORS from "../../../constants/colors";
 import DashedButton from "./components/DashedButton";
+import { useState } from "react";
 
-const data = [
+let data = [
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28b1",
     img: image,
     title: "Gallery1",
     featured: true,
-    imageCount: "",
   },
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28b2",
     img: image_sm,
     title: "Gallery2",
     featured: false,
-    imageCount: "",
   },
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28b3",
     img: image_sm,
     title: "Gallery3",
     featured: false,
-    imageCount: "",
   },
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28b4",
     img: image_sm,
     title: "Gallery4",
     featured: false,
-    imageCount: "",
   },
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28b5",
     img: image_sm,
     title: "Gallery5",
     featured: false,
-    imageCount: "",
   },
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28b6",
     img: image_sm,
     title: "Gallery6",
     featured: false,
-    imageCount: "",
   },
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28b7",
     img: image_sm,
     title: "Gallery7",
     featured: false,
-    imageCount: "12+",
   },
 ];
+
 export default function ImageGallery() {
   const classes = styles();
   const typoClasses = useTypographyStyles();
   const preventDefault = (event) => event.preventDefault();
+  const countType = () => {
+    const image = {
+      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28b7",
+      img: image_sm,
+      title: "Gallery7",
+      featured: false,
+    };
+    setData([...imageData, image]);
+    console.log(data);
+  };
 
-  ///commit
+  const [imageData, setData] = useState(data);
+
   return (
     <Paper className={classes.root} elevation={0}>
       <GridList cols={3} className={classes.gridList} cellHeight="auto">
-        {data.map((items) => (
+        {imageData.map((items, index) => (
           <GridListTile
             key={items.id}
             className={classes.imgGallery}
             cols={items.featured ? 3 : 1}
             rows={items.featured ? 2 : 1}
           >
-            <img className={classes.img} src={items.img} alt={items.title} />
-            <ButtonBase
-              onClick={preventDefault}
-              className={classes.linkGallery}
-            >
-              {items.imageCount}
-            </ButtonBase>
+            {index < 7 ? (
+              <img className={classes.img} src={items.img} alt={items.title} />
+            ) : null}
+            {index === 6 ? (
+              <ButtonBase
+                onClick={preventDefault}
+                className={classes.linkGallery}
+              >
+                + {imageData.length}
+              </ButtonBase>
+            ) : null}
           </GridListTile>
         ))}
       </GridList>
@@ -98,6 +108,7 @@ export default function ImageGallery() {
           id="contained-button-file"
           type="file"
           accept="img/*"
+          onClick={countType}
         />
         <DashedButton
           component="span"
@@ -137,6 +148,8 @@ const styles = makeStyles(() => ({
   linkGallery: {
     color: "#000",
     position: "absolute",
+    opacity: 0.5,
+    backgroundColor: "white",
     zIndex: 1,
     top: 0,
     width: "100%",
